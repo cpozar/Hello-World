3d8f7574a565842d8de8151671682d243a56109f
cpozar <cheyennepozar@gmail.com>
2020-10-13 13:21:40 -0700

    added main message that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Created by Cheyenne Pozar on 10/13/20
  */
 public class HelloWorld {
+    public static void main(String[] args)  {
+        System.out.println("Hello, World!");
+    }
 }
